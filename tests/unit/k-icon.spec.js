b3df6eb8d2676fedbc2a194fe0c1d7a110d49db7
lb <863379105@qq.com>
2020-11-22 05:09:47 +0800

    添加k-icon单元测试

---
+++ tests/unit/k-icon.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import kIcon from '@/components/k-icon.vue'
+
+describe('k-icon.vue', () => {
+  it('测试组件k-icon传参是否正常', () => {
+    const name = 'setting'
+    const wrapper = shallowMount(kIcon, {
+      propsData: { name }
+    })
+    expect(wrapper.element.querySelector('use').getAttribute('xlink:href')).toBe(`#icon-${name}`)
+  })
+})
